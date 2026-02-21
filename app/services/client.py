22185331df00dd627f5afdcb86dc3fe7864b05ea
filipe2710 <filipebrndao@gmail.com>
2026-02-20 22:27:15 -0300

    feat: add ClientService with in-memory client creation

---
+++ app/services/client.py
@@ -0,0 +1,11 @@
+from typing import List
+from app.schemas.client import ClientCreate
+
+class ClientService:
+  """Class control services of clients"""
+  
+  def __init__(self):
+    self.clients: List[ClientCreate] = []
+    
+  def create_client(self, client: ClientCreate):
+    self.clients.append(client)
