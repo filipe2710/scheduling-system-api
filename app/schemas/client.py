5b7b0250ff2b0071e7e702b538ea8740e062cca0
filipe2710 <filipebrndao@gmail.com>
2026-02-21 15:41:54 -0300

    refactor: change instance method to classmethod (self -> cls)

--- app/schemas/client.py
+++ app/schemas/client.py
@@ -14,23 +14,28 @@ class ClientCreate(BaseModel):
   
   @field_validator("gender", mode="before")
   @classmethod
-  def formalization_gender(self, gender: str) -> str:
-    gender = gender.strip().lower().capitalize()
+  def formalization_gender(cls, value: str) -> str:
+    value = value.strip().lower().capitalize()
+    
+    if value not in ("Masculino", "Feminino"):
+      raise ValueError("gender must be Masculino or Feminino")
+    
+    return value
   
   @field_validator("phone", mode="before")
   @classmethod
-  def formalization_phone(self, phone: str) -> str:
-    phone = re.sub(r"\b", "", phone)
+  def formalization_phone(cls, value: str) -> str:
+    phone = re.sub(r"\D", "", value)
     
     if len(phone) not in (10, 11):
       raise ValueError("phone must have 10 or 11 digits")
     
     return phone
   
-  @field_validator("phone", mode="before")
+  @field_validator("cpf", mode="before")
   @classmethod
-  def formalize_cpf(cls, cpf: str) -> str:
-    cpf = re.sub(r"\D", "", cpf)
+  def formalize_cpf(cls, value: str) -> str:
+    cpf = re.sub(r"\D", "", value)
     
     if len(cpf) != 11:
       raise ValueError("cpf must have 11 digits")
