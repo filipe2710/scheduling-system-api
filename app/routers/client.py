1ca47339eda409b4dd26c9e48fb4be843c5c74c3
filipe2710 <filipebrndao@gmail.com>
2026-02-24 18:33:54 -0300

    feat: return ClientRead with generated id in client endpoints

--- app/routers/client.py
+++ app/routers/client.py
@@ -1,23 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.schemas.client_create import ClientCreate
+from app.schemas.client_read import Client_Read
 from app.services.client import ClientService
 
 router = APIRouter(prefix="/clients", tags=["clients"])
 
 client_service = ClientService()
 
-@router.post("/")
+@router.post("/", response_model=Client_Read, status_code=status.HTTP_201_CREATED)
 def create_client(client: ClientCreate):
   """Create a new client"""
   
   client_service.create_client(client)
   
-  return {
-    "message": "client created successfully",
-    "client": client,
-  }
   
-@router.get("/")
+@router.get("/", response_model=list[Client_Read])
 def get_clients():
   return client_service.list_client()
--- app/services/client.py
+++ app/services/client.py
@@ -1,14 +1,32 @@
 from typing import List
+from pydantic import Field
+from uuid import uuid4
 from app.schemas.client_create import ClientCreate
+from app.schemas.client_read import Client_Read
 
 class ClientService:
   """Class control services of clients"""
   
   def __init__(self):
-    self.clients: List[ClientCreate] = []
+    self.clients: List[Client_Read] = []
     
-  def create_client(self, client: ClientCreate):
-    self.clients.append(client)
+  def create_client(self, client: ClientCreate) -> Client_Read:
+    client_read = Client_Read(
+      id=str(uuid4()),
+      name=client.name,
+      phone=client.phone,
+      date_of_birth=client.date_of_birth,
+      cpf=client.cpf,
+      gender=client.gender,
+      age=self._calculate_age(client.date_of_birth)
+    )
     
-  def list_client(self):
+    self.clients.append(client_read)
+    return client_read
+    
+  def list_client(self) -> list[Client_Read]:
     return self.clients
+  
+  def _calculate_age(self, date_of_birth):
+    pass
+  
