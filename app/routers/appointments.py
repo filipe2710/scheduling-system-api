5528b8c3521911f769ed0e38d4a219ac620064d5
filipe2710 <filipebrndao@gmail.com>
2026-03-07 17:36:50 -0300

    refactor: update appointment routes to use read and update schemas

--- app/routers/appointments.py
+++ app/routers/appointments.py
@@ -1,24 +1,21 @@
 from fastapi import APIRouter, HTTPException, status
 
-from app.schemas.appointment import AppointmentCreate
+from app.schemas.appointment import AppointmentCreate, AppointmentRead, AppointmentUpdate
 from app.services.scheduling_seruice import SchedulingService
 
 router = APIRouter(prefix="/appointments", tags=["Appointments"])
 
 scheduling_service = SchedulingService()
 
-@router.post("/", response_model=AppointmentCreate, status_code=status.HTTP_201_CREATED, summary="Create a new appointment", description="Create a new appointment after validating availability")
+@router.post("/", response_model=AppointmentRead, status_code=status.HTTP_201_CREATED, summary="Create a new appointment", description="Create a new appointment after validating availability")
 def create_appointment(appointment: AppointmentCreate):
   if not scheduling_service.validate_time_range(appointment):
     raise HTTPException(status_code=400, detail="Start time must be before end time")
  
   scheduling_service.create_appointment(appointment)
   
-  return {
-    "message": "appointment created successfully",
-    "appointment": appointment,
-  }
-  
-@router.get("/")
+  return appointment
+
+@router.get("/", response_model=list[AppointmentRead], status_code=status.HTTP_200_OK, summary="List all appointments", description="List all appointments")
 def get_appointments():
   return scheduling_service.list_appointment()
