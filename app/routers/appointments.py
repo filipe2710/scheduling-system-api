1963a04e31bd5b62f022cd787675c81c0c8ee8ca
filipe2710 <filipebrndao@gmail.com>
2026-03-13 20:26:11 -0300

    refactor: simplify appointment router and delegate logic to AppointmentService

--- app/routers/appointments.py
+++ app/routers/appointments.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter, HTTPException, status
 
 from app.schemas.appointment import AppointmentCreate, AppointmentRead, AppointmentUpdate
-from app.services.scheduling_seruice import SchedulingService
+from app.services.appointment_sevice import AppointmentService
 
 router = APIRouter(prefix="/appointments", tags=["Appointments"])
 
-scheduling_service = SchedulingService()
+appointment_service = AppointmentService()
 
 @router.post("/", response_model=AppointmentRead, status_code=status.HTTP_201_CREATED, summary="Create a new appointment", description="Create a new appointment after validating availability")
 def create_appointment(appointment: AppointmentCreate):
-  if not scheduling_service.validate_time_range(appointment):
+  if not appointment_service.validate_time_range(appointment):
     raise HTTPException(status_code=400, detail="Start time must be before end time")
  
-  scheduling_service.create_appointment(appointment)
+  appointment_service.create_appointment(appointment)
   
   return appointment
 
 @router.get("/", response_model=list[AppointmentRead], status_code=status.HTTP_200_OK, summary="List all appointments", description="List all appointments")
 def get_appointments():
-  return scheduling_service.list_appointment()
+  return appointment_service.list_appointment()
